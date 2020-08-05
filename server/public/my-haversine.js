c61c171757dc1a641141bd0f3b89d736a1f2cc5e
Simon Cooper <30930833+simoncoop1@users.noreply.github.com>
2020-08-05 21:50:11 +0100

    Add files via upload

---
+++ server/public/my-haversine.js
@@ -0,0 +1,19 @@
+function myHaversine(){
+    var long1 = -1.45723343;
+    var lat1 = 52.27519523;
+    var long2 = -1.45723343;
+    var lat2 = 52.1;
+    var ψ1 = lat1 * (Math.PI / 180);
+    var ψ2 = lat2 * (Math.PI/180);
+    var λ1 = long1 * (Math.PI/180);
+    var λ2 = long2 * (Math.PI/180);
+    var Δψ = ψ2 - ψ1;
+    var Δλ = λ2 - λ1;
+    var R = 6371; // radius of earth (km)
+
+    var a = (Math.sin(Δψ/2) **2) + (Math.cos(ψ1) * Math.cos(ψ2) * (Math.sin(Δλ/2) **2));
+    var c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+    var d = R * c;
+
+    return d;
+}
